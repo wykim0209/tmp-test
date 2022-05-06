699f35b5e56be7c7e1404b370a4182f100152f50
wykim0209 <wykim1989@gmail.com>
2022-05-06 14:46:56 +0900

    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class TmpClass:
+    self.name = "James"
+    self.age = 24
+
+if __name__ == "__main__":
+    tmp_object = TmpClass()
+    print("name:", tmp_object.name)
+    print("name:", tmp_object.age)
+    
